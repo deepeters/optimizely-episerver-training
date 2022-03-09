44ddb7996fbf63f180bc93e78c3b2be55d8316eb
deepeters <dennis95peters@gmail.com>
2022-03-09 15:47:34 +0300

    Start a new empty project 'AlloyTraining' and implement modifications one by one

---
+++ AlloyTraining/Models/Pages/StartPage.cs
@@ -0,0 +1,23 @@
+﻿using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+using EPiServer.SpecializedProperties;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace AlloyTraining.Models.Pages
+{
+    [ContentType(DisplayName = "StartPage", GUID = "cfb56fe8-8d63-4d5f-9b00-43667c91a485", Description = "")]
+    public class StartPage : PageData
+    {
+        /*
+                [CultureSpecific]
+                [Display(
+                    Name = "Main body",
+                    Description = "The main body will be shown in the main content area of the page, using the XHTML-editor you can insert for example text, images and tables.",
+                    GroupName = SystemTabNames.Content,
+                    Order = 1)]
+                public virtual XhtmlString MainBody { get; set; }
+         */
+    }
+}
